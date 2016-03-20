9f609ac9a0c07ab701acee2eea1139c25eaa049f
Eko Wahyudi <eko.wahyudi15@outlook.com>
2016-03-20 09:27:23 +0700

    Initial commit - just java

---
+++ app/src/main/java/com/digitcreativestudio/justjava/MainActivity.java
@@ -0,0 +1,65 @@
+package com.digitcreativestudio.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        String price = "$"+ (quantity * 5);
+        price = price + "\nThank You!";
+        displayPrice(price);
+    }
+
+    /**
+     * This method is called when the increment button is clicked.
+     */
+    public void incrementOrder(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the increment button is clicked.
+     */
+    public void decrementOrder(View view) {
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(String a) {
+
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        //priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText(a);
+    }
+}
